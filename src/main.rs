cd79e9991e9660cac72815b1a61d7ac2f3eaba8a
tuck3rb <119343799+tuck3rb@users.noreply.github.com>
2023-03-16 16:59:36 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,48 @@
 #![no_std]
 #![no_main]
 
-mod vga_buffer;
-mod serial;
-
 use pc_keyboard::DecodedKey;
 use pluggable_interrupt_os::HandlerTable;
-
-fn start() {
-    println!("Hello, world!");
-}
-
-fn tick() {
-    print!(".");
-}
-
-fn key(key: DecodedKey) {
-    match key {
-        DecodedKey::Unicode(character) => print!("{}", character),
-        DecodedKey::RawKey(key) => print!("{:?}", key),
-    }
-}
+use BareMetalGame::Game;
+use crossbeam::atomic::AtomicCell;
+use pluggable_interrupt_os::vga_buffer::clear_screen;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     HandlerTable::new()
         .keyboard(key)
         .timer(tick)
-        .startup(start)
+        .startup(startup)
+        .cpu_loop(cpu_loop)
         .start()
+}
+
+static LAST_KEY: AtomicCell<Option<DecodedKey>> = AtomicCell::new(None);
+static TICKS: AtomicCell<usize> = AtomicCell::new(0);
+
+fn cpu_loop() -> ! {
+    let mut kernel = Game::new();
+    let mut last_tick = 0;
+    loop {
+        if let Some(key) = LAST_KEY.swap(None) {
+            kernel.key(key);
+        }
+        let current_tick = TICKS.load();
+        if current_tick > last_tick {
+            last_tick = current_tick;
+            kernel.tick();
+        }
+    }
+}
+
+fn tick() {
+    TICKS.fetch_add(1);
+}
+
+fn key(key: DecodedKey) {
+    LAST_KEY.store(Some(key));
+}
+
+fn startup() {
+    clear_screen();
 }
